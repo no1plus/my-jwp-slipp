354f0f7050b1f04ea4f59413fe59cb33c9bc8daf
brad.choi <norangoguma@gmail.com>
2016-10-18 00:31:41 +0900

    Slipp jsp 스터디 13장

---
+++ slipp/src/net/slipp/db/Database.java
@@ -0,0 +1,11 @@
+package net.slipp.db;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.slipp.user.User;
+
+public class Database {
+	
+	private static Map<String, User> users = new HashMap<String, User>();
+}
---
+++ slipp/src/net/slipp/user/User.java
@@ -0,0 +1,61 @@
+package net.slipp.user;
+
+public class User {
+	private String name;
+	private String password;
+	private String userId;
+	private String email;
+	
+	
+	public User(String name, String password, String userId, String email) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.userId = userId;
+		this.email = email;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
